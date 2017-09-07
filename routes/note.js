8f02707ab8a1b49e978f18cbf4fb75b6f630d62c
guokui <guokui@vc265.com>
2017-09-07 14:47:13 +0800

    update

--- routes/note.js
+++ routes/note.js
@@ -5,55 +5,69 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
-const COMMENTS_FILE = "E:/MyProject/p-ablum/public/note/content.txt";
+const nodeExcel = require('excel-export');
+// const COMMENTS_FILE = "E:/MyProject/p-ablum/public/note/content.txt";
 
 const noteTpl = require("../views/note/showNote.marko");
 
 var resolve = file => path.resolve(__dirname, file);
 
+//获取 年-月-日 当做文件名
 function getNowFormatDate() {
-  var date = new Date();
-  // var seperator1 = "-";
-  // var seperator2 = ":";
-  var month = date.getMonth() + 1;
-  var strDate = date.getDate();
+  let date = new Date();
+  // let seperator = "-";
+  let month = date.getMonth() + 1;
+  let strDate = date.getDate();
   if (month >= 1 && month <= 9) {
     month = "0" + month;
   }
   if (strDate >= 0 && strDate <= 9) {
     strDate = "0" + strDate;
   }
-  var currentdate = date.getFullYear() + month + strDate;
+  let currentdate = date.getFullYear() + month + strDate;
   return currentdate;
 }
 
+//获取 时-分-秒 记录提交数据时间
+function getNowTime() {
+  let date = new Date();
+  let seperator = ":";
+  let currentdate = date.getHours() + seperator + date.getMinutes() + seperator + date.getSeconds();
+  return currentdate;
+
+}
+
 router.get('/', function (req, res, next) {
-  noteTpl.render({},res);
+  let timeStamp = getNowFormatDate();
+  let folder = 'note/'+ timeStamp +'.txt';
+  let pathStr = resolve(folder);
+  let noteList = '';
+  if (!fs.existsSync(pathStr)){
+    fs.writeFile(pathStr,'[]',function (err,data) {
+      if (err){
+        console.log("写入失败");
+      }
+    })
+  }else {
+    fs.readFile(pathStr, (err, data) => {
+      if(err){
+        console.log(err);
+        process.exit(1);
+      }
+      console.log(JSON.parse(data));
+      noteList = JSON.parse(data);
+      noteTpl.render({noteList},res);
+    })
+  }
 });
 
 router.post('/_save',function (req, res, next) {
   console.log(req.body);
   let timeStamp = getNowFormatDate();
-  console.log(timeStamp);
-  var folder = 'note/test.txt';
-  console.log(resolve(folder));
-  var pathStr = resolve(folder);
-  if (!fs.existsSync(pathStr)){
-    fs.mkdirSync(pathStr);
-  }
-  fs.readFile(pathStr,function (err,data) {
-    if (err){
-      console.log(err);
-      return console.log("读取文件失败");
-    }else {
-      fs.writeFile(pathStr,"nihao",function (err,data) {
-        if (err){
-          return console.log("写入失败");
-        }
-      })
-    }
-  })
-  fs.readFile(COMMENTS_FILE, (err, data) => {
+  let folder = 'note/'+ timeStamp +'.txt';
+  let pathStr = resolve(folder);
+
+  fs.readFile(pathStr, (err, data) => {
     if(err){
       console.log(err);
       process.exit(1);
@@ -65,10 +79,10 @@ router.post('/_save',function (req, res, next) {
       phone:req.body.phone,
       question:req.body.question,
       answer:req.body.answer,
-      time:req.body.time
+      time:getNowTime()
     };
     comments.push(newComment);
-    fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), (err, data) => {
+    fs.writeFile(pathStr, JSON.stringify(comments, null, 4), (err, data) => {
       if(err){
         console.log(err);
         process.exit(1);
@@ -78,5 +92,49 @@ router.post('/_save',function (req, res, next) {
   });
 });
 
+/**
+ * 下载文件
+ */
+router.get("/downLoad",function (req, res, next) {
+
+  let comments = "";
+  console.log(req.body);
+  let timeStamp = getNowFormatDate();
+  let folder = 'note/'+ timeStamp +'.txt';
+  let pathStr = resolve(folder);
+  fs.readFile(pathStr, (err, data) => {
+    if(err){
+      console.log(err);
+      process.exit(1);
+    }
+    comments = JSON.parse(data);
+
+    let conf ={};
+    conf.cols = [
+      {caption: '访客ID', type: 'string'},
+      {caption: '访客姓名', type: 'string'},
+      {caption: '联系方式', type: 'string'},
+      {caption: '反馈问题', type: 'string'},
+      {caption: '解决方案', type: 'string'},
+      {caption: '提交时间', type: 'string'}
+    ];
+
+    let arr = [];
+    for (let comment of comments){
+      let arrComment = [comment.id,comment.name,comment.phone,comment.question,comment.answer,comment.time];
+      arr.push(arrComment);
+    }
+
+    console.log(arr);
+    conf.rows = arr;
+    let result = nodeExcel.execute(conf);
+    res.setHeader('Content-Type', 'application/vnd.openxmlformats');
+    res.setHeader("Content-Disposition", "attachment; filename=" + timeStamp +".xlsx");
+
+    res.end(result, 'binary');
+
+  });
+
+})
 
 module.exports = router;
